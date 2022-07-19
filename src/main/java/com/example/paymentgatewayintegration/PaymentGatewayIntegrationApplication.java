ac8ac757db1af1686f45f53c21786dec6e24aa3c
omarsaeed2466 <47633542+omarsaeed2466@users.noreply.github.com>
2022-07-19 06:26:36 +0200

    init commit : How to perform payment integration using paypal

---
+++ src/main/java/com/example/paymentgatewayintegration/PaymentGatewayIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.paymentgatewayintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentGatewayIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentGatewayIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/paymentgatewayintegration/PaypalController.java
@@ -0,0 +1,60 @@
+package com.example.paymentgatewayintegration;
+
+import com.paypal.api.payments.Links;
+import com.paypal.api.payments.Payment;
+import com.paypal.base.rest.PayPalRESTException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class PaypalController {
+   @Autowired
+    private PaypalServices Service;
+    public static final String SUCCESS_URL="pay/success";
+    public static final String CANCEL_URL="pay/cancel";
+    @GetMapping("/")
+    public String home(){
+        return "home";
+
+    }
+    @PostMapping ("/pay")
+    public String payment(@ModelAttribute("order") Order order){
+        try {
+            Payment payment = Service.createPayment(order.getPrice(),order.getCurrency(),
+                    order.getMethod(),order.getIntent(),order.getDescription(),"http://localhost:9090/"+CANCEL_URL,
+                    "http://localhost:9090/"+SUCCESS_URL);
+            for (Links link:payment.getLinks()){
+                if (link.getRel().equals("approval_url")){
+                    return "redirect:"+link.getHref();
+
+                }
+            }
+        } catch (PayPalRESTException e) {
+            e.printStackTrace();
+        }
+        return "redirect:/";
+    }
+
+
+    @GetMapping(value = "CANCEL_URL")
+    public String cancelPay(){
+
+        return "cancel";
+    }
+
+
+    @GetMapping(value ="SUCCESS_URL")
+    public  String successPay(@RequestParam("PaymentId") String paymentId,@RequestParam("PayerId") String payerId){
+try{
+    Payment payment = Service.executePayment(paymentId,payerId);
+    System.out.println(payment.toJSON());
+    if (payment.getState().equals("approved")){
+        return "success";
+    }
+}catch (PayPalRESTException e){
+    System.out.println(e.getMessage());
+}
+return "redirect:/";
+    }
+}
